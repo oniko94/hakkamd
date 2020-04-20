469b2cbf7591cf1399692b6c5ca4826ede0e2f7f
AlexNikolaiev94 <oleksandr.nikolaiev94@gmail.com>
2020-04-20 11:36:17 +0300

    Set up FormattingTags test and refactor after testing

--- src/FormattingTags.js
+++ src/FormattingTags.js
@@ -1,34 +1,39 @@
 const FormattingTags = {
     Blockquote(text) {
-        return `<blockquote>${text.slice(1)}</blockquote>`;
+        return `<blockquote>${text.slice(1).trim()}</blockquote>`;
     },
     Bold(text) {
-        return `<b>${text.slice(1, -1)}</b>`;
+        return `<b>${text.slice(1, -1).trim()}</b>`;
     },
     Code(text) {
-        let apostrophes = 0,
+        let i = 0,
             template = '';
+        text = text.trim();
+
         while (text.startsWith('`')) {
             text = text.slice(1);
-            apostrophes++;
+            if (text.endsWith('`')) {
+                text = text.slice(0, -1);
+            }
+            i++;
 
-            if (apostrophes >= 3) {
-                template = `<pre>${text.slice(3, -3)}</pre>`;
+            template = `<code>${text}</code>`;
+
+            if (i === 3) {
+                template = `<pre>${text}</pre>`;
                 break;
-            } else {
-                template = `<code>${text.slice(1, -1)}</code>`;
             }
         }
         return template;
     },
     Italic(text) {
-        return `<i>${text.slice(1, -1)}</i>`;
+        return `<i>${text.slice(1, -1).trim()}</i>`;
     },
     Spoiler(text) {
-        return `<span class="spoiler">${text.slice(1, -1)}</span>`;
+        return `<span class="spoiler">${text.slice(1, -1).trim()}</span>`;
     },
     Strikethrough(text) {
-        return `<s>${text.slice(1, -1)}</s>`;
+        return `<s>${text.slice(1, -1).trim()}</s>`;
     }
 };
 
